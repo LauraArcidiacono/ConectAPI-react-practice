a8dfd796635a3973c8c475c173949a8e087d7a08
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-12-27 13:01:52 +0100

    Update App component
    Remove useState and useEffect
    Add keyWord prop to ListOfGifs

--- conect-api-react-practice/src/App.jsx
+++ conect-api-react-practice/src/App.jsx
@@ -1,19 +1,12 @@
-import { React, useState, useEffect } from 'react';
+import { React } from 'react';
 import './App.css';
-import getGifs from './services/getGifs';
 import ListOfGifs from './components/ListOfGifs';
 
 export default function App() {
-  const [gifs, setGifs] = useState([]);
-
-  useEffect(() => {
-    getGifs({ keyWord: 'susie+myerson' }).then((gifsArray) => setGifs(gifsArray));
-  }, ([]));
-
   return (
     <div className="App">
       <section className="gif__item">
-        <ListOfGifs gifs={gifs} />
+        <ListOfGifs keyWord="mrs+maisel" />
       </section>
     </div>
   );
