7d51eeabec7c5f5839278125680c3dc92bb02346
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-12-27 12:18:17 +0100

    Create Gif component

---
+++ conect-api-react-practice/src/components/Gif.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Gif({ id, title, url }) {
+  return (
+    <div>
+      <h3>{ title }</h3>
+      <small>
+        {id}
+        {' '}
+      </small>
+      <img
+        src={url}
+        alt={title}
+        className="gif__item"
+      />
+    </div>
+  );
+}
