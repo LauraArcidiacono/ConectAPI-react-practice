bb3d014d0aaad14541e34a8ea17b5fd8239538c8
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-12-27 13:36:38 +0100

    Update App
    Change keyWord with keyword
    Import and use Router and Link from 'wouter'

--- conect-api-react-practice/src/App.jsx
+++ conect-api-react-practice/src/App.jsx
@@ -1,12 +1,19 @@
 import { React } from 'react';
 import './App.css';
+import { Route, Link } from 'wouter';
 import ListOfGifs from './components/ListOfGifs';
 
 export default function App() {
   return (
     <div className="App">
-      <section className="gif__item">
-        <ListOfGifs keyWord="mrs+maisel" />
+      <section className="App__listOfGifs">
+        <h1>Connect React + API practice</h1>
+        <Link to="/gif/mrs+maisel">Mrs Maisel</Link>
+        <Link to="/gif/susie+myerson">Susie</Link>
+        <Route
+          component={ListOfGifs}
+          path="/gif/:keyword"
+        />
       </section>
     </div>
   );
