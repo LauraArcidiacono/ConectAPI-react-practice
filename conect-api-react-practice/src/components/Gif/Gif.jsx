c6420075961869614969ab3747efd82eea02e42d
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-12-28 10:54:04 +0100

    Add destructuring to Link import

--- conect-api-react-practice/src/components/Gif/Gif.jsx
+++ conect-api-react-practice/src/components/Gif/Gif.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import Link from 'wouter';
+import { Link } from 'wouter';
 import './Gif.css';
 
 export default function Gif({ id, title, url }) {
   return (
     <div className="Gif">
       <Link to={`gif/${id}`} className="Gif-link">
-        <h3>{ title }</h3>
         <img
           src={url}
           alt={title}
